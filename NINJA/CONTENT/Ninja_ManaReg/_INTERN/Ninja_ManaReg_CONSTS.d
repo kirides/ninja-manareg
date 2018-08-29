f1bb955c11c4e7747605855b2281e0d636de6c47
Kirides <john.heckendorf@outlook.com>
2018-08-29 22:06:15 +0200

    Added INI configuration for Gothic.ini
    # Example (Gothic.ini)
    Add to the bottom of Gothic.ini / set values
    
    [NINJA_MANAREG]
    THRESHOLD=50
    DIVISOR=20

--- NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_CONSTS.d
+++ NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_CONSTS.d
@@ -1,5 +1,12 @@
 const int NINJA_MANAREG_LEGO_FLAGS = LeGo_FrameFunctions | LeGo_Timer;
-const string NINJA_MANAREG_VERSION = "ManaReg 1.0.1";
+const string NINJA_MANAREG_VERSION = "ManaReg 1.1.0";
 
-const int NINJA_MANAREG_MANA_THRESHOLD = 50;
-const int NINJA_MANAREG_MAX_MANA_DIVISOR = 50;
+const string NINJA_MANAREG_INI = "NINJA_MANAREG";
+const string NINJA_MANAREG_INI_THRESHOLD = "THRESHOLD";
+const string NINJA_MANAREG_INI_DIVISOR = "DIVISOR";
+
+const int DEFAULT_NINJA_MANAREG_MANA_THRESHOLD = 50;
+const int DEFAULT_NINJA_MANAREG_MAX_MANA_DIVISOR = 50;
+
+var int Ninja_ManaReg_Mana_Threshold;
+var int Ninja_ManaReg_Max_Mana_Divisor;
--- NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_REGENERATION.d
+++ NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_REGENERATION.d
@@ -7,9 +7,9 @@ func void Ninja_ManaReg_Regeneration() {
 
     var c_npc n; n = Hlp_GetNpc(hero);
     
-    if (n.attribute[ATR_MANA_MAX] >= NINJA_MANAREG_MANA_THRESHOLD) {
+    if (n.attribute[ATR_MANA_MAX] >= Ninja_ManaReg_Mana_Threshold) {
         if (n.attribute[ATR_MANA] < n.attribute[ATR_MANA_MAX]) {
-            var int menge; menge = (n.attribute[ATR_MANA_MAX] + (NINJA_MANAREG_MAX_MANA_DIVISOR/2)) / NINJA_MANAREG_MAX_MANA_DIVISOR;
+            var int menge; menge = (n.attribute[ATR_MANA_MAX] + (Ninja_ManaReg_Max_Mana_Divisor/2)) / Ninja_ManaReg_Max_Mana_Divisor;
             Npc_ChangeAttribute(n, ATR_MANA, menge);
         };
     };
