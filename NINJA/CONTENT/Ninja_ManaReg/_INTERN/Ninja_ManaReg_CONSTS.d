1030e0e9cb456d9c8399df9472f4209685c6236b
Kirides <john.heckendorf@outlook.com>
2018-08-29 20:55:01 +0200

    Refactor and optimizations
    - Use integer math (perf)
    - constants for threshold and divisor
    - fix casing for flag constant
    - rename leftover-files from *GFA to *ManaReg

--- NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_CONSTS.d
+++ NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_CONSTS.d
@@ -1,2 +1,5 @@
-const int Ninja_ManaReg_LeGo_Flags = LeGo_FrameFunctions | LeGo_Timer;
-const string NINJA_MANAREG_VERSION = "ManaReg 0.1";
+const int NINJA_MANAREG_LEGO_FLAGS = LeGo_FrameFunctions | LeGo_Timer;
+const string NINJA_MANAREG_VERSION = "ManaReg 1.0.1";
+
+const int NINJA_MANAREG_MANA_THRESHOLD = 50;
+const int NINJA_MANAREG_MAX_MANA_DIVISOR = 50;
--- NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_REGENERATION.d
+++ NINJA/CONTENT/Ninja_ManaReg/_INTERN/Ninja_ManaReg_REGENERATION.d
@@ -1,14 +1,15 @@
-var int Ninja_ManaReg_Mana_Threshold;
-var int Ninja_ManaReg_MaxManaDivf;
 
 func void Ninja_ManaReg_Regeneration() {
-    var c_npc n; n = Hlp_GetNpc(PC_Hero);
-    
-    if (n.attribute[ATR_MANA_MAX] >= Ninja_ManaReg_Mana_Threshold) {
-        if (n.attribute[ATR_MANA] < n.attribute[ATR_MANA_MAX]) {
-            var int mreg_div; mreg_div = divf(mkf(n.attribute[ATR_MANA_MAX]), Ninja_ManaReg_MaxManaDivf);
-            var int menge; menge = roundf(mreg_div);
+    // HACK: Entfernen wenn es FF gibt, welche nur im nicht pausierten Modus feuern.
+    if (!mem_game.timestep) {
+        return;
+    };
 
+    var c_npc n; n = Hlp_GetNpc(hero);
+    
+    if (n.attribute[ATR_MANA_MAX] >= NINJA_MANAREG_MANA_THRESHOLD) {
+        if (n.attribute[ATR_MANA] < n.attribute[ATR_MANA_MAX]) {
+            var int menge; menge = (n.attribute[ATR_MANA_MAX] + (NINJA_MANAREG_MAX_MANA_DIVISOR/2)) / NINJA_MANAREG_MAX_MANA_DIVISOR;
             Npc_ChangeAttribute(n, ATR_MANA, menge);
         };
     };
